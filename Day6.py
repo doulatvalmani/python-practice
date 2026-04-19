4396b03ebb1a1bc7e0eca26699dd712bf4e9b2b8
doulatvalmani <doulatvalmani@gmail.com>
2026-04-19 16:16:56 +0500

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -1,49 +1,4 @@
 python
-# 
-contact = {
-    "name": "Doulat",
-    "email": "doulat@gmail.com",
-    "city": "Hyderabad",
-    "goal": "AWS Freelancer"
-}
-
-print(contact["name"])
-print(contact["email"])
-print(contact["city"])
-print(contact["goal"])
-
-print("Hello Brothers, My name is", contact["name"], "and I am from", contact["city"], ". I am an aspiring", contact["goal"], "and my email is", contact["email"])
-
-for key, value in contact.items():
-    print(key, ":", value)
-
-
-
-python
-# 
-contact = {
-    "name": "Doulat",
-    "email": "doulat@gmail.com",
-    "city": "Hyderabad",
-    "goal": "AWS Freelancer",
-    "skill": "Python",
-    "experience_months": 0
-}
-
-print(contact["name"])
-print(contact["email"])
-print(contact["city"])
-print(contact["goal"])
-print(contact["skill"])
-print(contact["experience_months"])
-
-print("Hello Brothers, My name is", contact["name"], "and I am from", contact["city"], ". I am an aspiring", contact["goal"], "and my email is", contact["email"])
-
-for key, value in contact.items():
-    print(key, ":", value)
-
-
-python
 #
 contacts = [
     {"name": "Ali",  "email": "ali@gmail.com",  "subscribed": True},
