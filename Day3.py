5c220492235986199d6edbd12f18c3c358c9273b
doulatvalmani <doulatvalmani@gmail.com>
2026-04-19 15:56:16 +0500

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,88 @@
+python
+# 
+success_rate = 95
+if success_rate > 90:
+    print("Excellent Campaign.")
+elif success_rate > 70:
+    print("Good Campaign.")
+else:
+    print("Campaign Needs Improvement.")
+
+
+python
+# 
+success_rate = 80
+if success_rate > 90:
+    print("Excellent Campaign.")
+elif success_rate > 70:
+    print("Good Campaign.")
+else:
+    print("Campaign Needs Improvement.")
+
+
+python
+# 
+success_rate = 60
+if success_rate > 90:
+    print("Excellent Campaign.")
+elif success_rate > 70:
+    print("Good Campaign.")
+else:
+    print("Campaign Needs Improvement.")
+
+
+python
+#
+experience_month = 0
+
+if experience_month <= 3:
+    print("Charge $10-15/hr, focus on reviews")
+elif experience_month <= 6:
+    print("Charge $15-25/hr, you have experience")
+elif experience_month <= 12:
+    print("Charge $25-40/hr, you are established")
+else:
+    print("Charge $40-60/hr, you are an expert")
+
+
+python
+#
+experience_month = 5
+
+if experience_month <= 3:
+    print("Charge $10-15/hr, focus on reviews")
+elif experience_month <= 6:
+    print("Charge $15-25/hr, you have experience")
+elif experience_month <= 12:
+    print("Charge $25-40/hr, you are established")
+else:
+    print("Charge $40-60/hr, you are an expert")
+
+
+python
+#
+experience_month = 8
+
+if experience_month <= 3:
+    print("Charge $10-15/hr, focus on reviews")
+elif experience_month <= 6:
+    print("Charge $15-25/hr, you have experience")
+elif experience_month <= 12:
+    print("Charge $25-40/hr, you are established")
+else:
+    print("Charge $40-60/hr, you are an expert")
+  
+
+python
+#
+experience_month = 15
+
+if experience_month <= 3:
+    print("Charge $10-15/hr, focus on reviews")
+elif experience_month <= 6:
+    print("Charge $15-25/hr, you have experience")
+elif experience_month <= 12:
+    print("Charge $25-40/hr, you are established")
+else:
+    print("Charge $40-60/hr, you are an expert")
+    
