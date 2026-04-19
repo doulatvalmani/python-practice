11b2fb1f72abd92616a262e077bc5982541c13ec
doulatvalmani <doulatvalmani@gmail.com>
2026-04-19 15:49:01 +0500

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,29 @@
+python
+#
+total__emails = 1000
+sent = 850
+failed = total__emails - sent
+succes__rate = (sent / total__emails) * 100
+print("success rate:", succes__rate, "%")
+print("failed rate:", 100 - succes__rate, "%")
+
+python
+#
+total_emails = 1000
+sent = 850
+failed = total_emails - sent
+succes_rate = (sent / total_emails) * 100
+print("success rate:", succes_rate, "%")
+print("failed rate:", 100 - succes_rate, "%")
+
+python
+#
+hourly_rate = 25
+hours_daily = 4
+days_per_month = 22
+daily_earnings = hourly_rate * hours_daily
+monthly_earnings = daily_earnings * days_per_month
+earning_in_pkr = monthly_earnings * 280
+print("Daily Earnings: $", daily_earnings)
+print("Monthly Earnings: $", monthly_earnings)
+print("Earnings in PKR: PKR", earning_in_pkr)
